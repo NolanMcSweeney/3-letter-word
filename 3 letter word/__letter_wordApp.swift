8fcef2024c04fe493920e79596b1bb70781513fe
Nolan McSweeney <nolanbm2009@icloud.com>
2024-10-16 23:55:53 -0500

    Initial Commit

---
+++ 3 letter word/__letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  __letter_wordApp.swift
+//  3 letter word
+//
+//  Created by Nolan Brian McSweeney on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct __letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3 letter wordTests/__letter_wordTests.swift
@@ -0,0 +1,17 @@
+//
+//  __letter_wordTests.swift
+//  3 letter wordTests
+//
+//  Created by Nolan Brian McSweeney on 10/16/24.
+//
+
+import Testing
+@testable import __letter_word
+
+struct __letter_wordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
