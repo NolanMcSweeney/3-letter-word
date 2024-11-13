01bfa463c1644d69e0c2053f5fefcb1ef19e7bea
Nolan McSweeney <nolanbm2009@icloud.com>
2024-11-13 09:44:35 -0600

    Finished

--- 3 letter word/ContentView.swift
+++ 3 letter word/ContentView.swift
@@ -6,19 +6,64 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    @State private var selectedLetters = ["", "", ""]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Three Letter Word")
+                .font(.title).bold()
+            Text("Tap the gray box to change the letter")
+            
+            
+            customLetterBox(color: .red, text: letter )
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
+            Text("Tap the red box to select that letter")
         }
-        .padding()
+        HStack {
+            ForEach(0..<3) { index in
+                customLetterBox(color: .red, text: selectedLetters[index])
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            selectedLetters[index] = letter
+                        }
+                        
+                    }
+            }
+      
+        }
+        .preferredColorScheme(.dark)
+       
     }
+        
 }
 
 #Preview {
     ContentView()
+}
+
+struct customLetterBox: View {
+    let color : Color
+    let text : String
+    var body: some View {
+        ZStack {
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+            
+        }
+        .frame(width: 120, height: 120)
+    }
 }
